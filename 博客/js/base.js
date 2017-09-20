46eb16cd199ae968cd3ed9693bdddc9738fe74b9
AnimationXin <1446854524@qq.com>
2017-09-20 17:55:30 +0800

    xin

--- 博客/js/base.js
+++ 博客/js/base.js
@@ -16,6 +16,40 @@ for(var i=0;i<aLi.length;i++){
        }
     }
 }
+var persion = new fis();
+persion.show(oBtn );
+function fis(){
+    fis.prototype.show = function (oBtn){
+    var timer = null;
+    var bFlag = false;
+    oBtn.onclick = function () {
+    moveScroll(0,3000);
+    }
+    window.onscroll = function (){
+    if(bFlag){
+        clearInterval(timer);
+    }
+    bFlag=true;
+    };
+    function moveScroll(target,time) {
+    var start = document.documentElement.scrollTop || document.body.scrollTop;
+    var dis = target - start;
+    var count = Math.floor(time/300);
+    var n=0;
+    clearInterval(timer);
+    timer = setInterval(function(){
+        bFlag=false;
+        n++;
+        document.documentElement.scrollTop = start + dis* n / count;
+        document.body.scrollTop = start + dis * n / count;
+        if(n==count){
+            clearInterval(timer);
+        }
+    },30)
+}
+
+    }
+}
 
 oBtn.onclick = function () {
     moveScroll(0,3000);
